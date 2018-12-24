452a4d4cd84011d5627d2649377a8dc0e897ace0
dbierer <doug@unlikelysource.com>
2018-12-24 13:47:04 +0700

    2018-12-24 DB: added insert for property collection

--- build_hotels_from_csv_and_real_data.php
+++ build_hotels_from_csv_and_real_data.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * builds $targetDb.$targetCollection MongoDB collection
+ * builds $targetDb.$targetCol MongoDB collection
  */
 require __DIR__ . '/vendor/autoload.php';
 use Application\Client;
@@ -14,23 +14,24 @@ define('SOURCE_FIRST_NAMES_MALE', __DIR__ . '/first_names_male.txt');
 define('SOURCE_FIRST_NAMES_FEMALE', __DIR__ . '/first_names_female.txt');
 
 // init vars
-$max = 300;         // target number of entries to generate
-$writeJs = TRUE;    // set this TRUE to output JS file to perform inserts
-$writeBson = FALSE; // set this TRUE to directly input into MongoDB database
-$sourceDb = 'source_data';
-$targetDb = 'booksomething';
-$targetCollection = 'property';
-$targetJs = __DIR__ . '/' . $targetDb . '_' . $targetCollection . '_insert.js';
+$max       = 300;       // target number of entries to generate
+$writeJs   = TRUE;      // set this TRUE to output JS file to perform inserts
+$writeBson = FALSE;     // set this TRUE to directly input into MongoDB database
+$sourceDb  = 'source_data';
+$targetDb  = 'booksomething';
+$targetCol = 'property';
+$targetJs  = __DIR__ . '/' . $targetDb . '_' . $targetCol . '_insert.js';
 $processed = 0;
 $inserted  = 0;
+$written   = 0;
 
 // set up javascript
 if ($writeJs) {
     $jsFile = new SplFileObject($targetJs, 'w');
     $outputJs = 'conn = new Mongo();' . PHP_EOL
               . 'db = conn.getDB("' . $targetDb . '");' . PHP_EOL
-              . 'db.' . $targetCollection . '.drop();' . PHP_EOL;
-    $openJs   = 'db.' . $targetCollection . '.insertOne(' . PHP_EOL;
+              . 'db.' . $targetCol . '.drop();' . PHP_EOL;
+    $openJs   = 'db.' . $targetCol . '.insertOne(' . PHP_EOL;
     $closeJs  = ');' . PHP_EOL;
     $jsFile->fwrite($outputJs);
     echo $outputJs;
@@ -43,7 +44,7 @@ $firstNamesFemale = file(SOURCE_FIRST_NAMES_FEMALE);
 $isoCodes         = file(SOURCE_COUNTRIES);
 $isps             = file(SOURCE_ISP);
 $socMedia         = ['GO' => 'google', 'TW' => 'twitter', 'FB' => 'facebook', 'LN' => 'line', 'SK' => 'skype','LI' => 'linkedin'];
-$name1            = ['Cozy','Riverside','Lakeside','Mountain','Rose','Garden','Valley','Castle','Sleepy','Amazing','Awesome','Romantic','Secluded','Peaceful','Restful','Quiet','Tranquil','Getaway','Take a Break','Famous'];
+$name1            = ['Cozy','Riverside','Lakeside','Mountain','Rose','Garden','Valley','Castle','Sleepy','Amazing','Awesome','Romantic','Secluded','Peaceful','Restful','Quiet','Tranquil','Getaway','Take a Break','Famous','Destination','Travel','Voyage'];
 $street1          = ['Winding','Blue','Red','Green','Big','Little','Long','Short'];
 $street2          = ['Bough','Tree','Bridge','Creek','River','Bend','Mountain','Hill','Ditch','Gulch','Gully','Canyon','Mound','Woods','Ridge','Stream'];
 $street3          = ['Way','Street','Boulevard','Avenue','Drive','Road','Circle','Ride'];
@@ -63,7 +64,7 @@ $roomNumber       = NULL;
 $branded = [];
 $brandedFile = new SplFileObject(SOURCE_BRANDED, 'r');
 while ($row = $brandedFile->fgetcsv())
-    if ($row && count($row))
+    if ($row && count($row) && $row[0])
         $branded[$row[0]] = array_slice($row, 1);
 
 try {
@@ -71,7 +72,7 @@ try {
     // set up mongodb client + collections
     $params = ['host' => '127.0.0.1'];
     $client = (new Client($params))->getClient();
-    $target = $client->$targetDb->$targetCollection;
+    $target = $client->$targetDb->$targetCol;
     $source = $client->$sourceDb;
 
     // build list of ISO codes
@@ -116,8 +117,6 @@ try {
             'propertyBrand' => $brand,
             'numberOfRooms' => rand(10,5000)
         ];
-        
-        var_dump($propertyInfo); continue;
         
         //*** build PrimaryContactInfo *********************************************************
         /*
@@ -169,29 +168,7 @@ try {
             'email'       => $email,
             'socialMedia' => $soc
         ];
-
-        //*** build RoomInfo *********************************************************
-        /*
-        "RoomInfo" : {
-            "roomType"        : <common::roomType>,
-            "roomLocation"    : <common::roomLocation>,
-            "price"           : <float>,
-            "bedType"         : [<common::bedType>],
-            "breakfast"       : <common::breakfast>,
-            "refundable       : <common::refundable>,
-            "discount"        : <float>
-        }
-        */
-        $roomInfo = [];
-        for ($x = 0; $x < rand(2, 8); $x++ ) {
-            $roomInfo[$x]['roomType']     = $roomType[array_rand($roomType)];
-            $roomInfo[$x]['roomLocation'] = $roomType[array_rand($roomLocation)];
-            $roomInfo[$x]['price']        = (float) rand(20,1000);
-            for ($y = 0; $y < rand(1, 3); $y++ )
-                $roomInfo[$x]['bedType'][] = $bedType[array_rand($bedType)];
-            $roomInfo[$x]['breakfast']    = $breakfast[array_rand($breakfast)];
-        }
-
+        
         //*** build Address *********************************************************
         /*
         "Address" : {
@@ -211,15 +188,15 @@ try {
         */
         
         // street address
-        $address = rand(1,9999) . ' ' 
+        $streetAddr = rand(1,9999) . ' ' 
                  . $street1[array_rand($street1)] . ' ' 
                  . $street2[array_rand($street2)] . ' ' 
                  . $street3[array_rand($street3)];
 
         // build buildingName, floor, etc.
-        $buildingName = (rand(1,10) === 1) ? 'Building ' . strtoupper(bin2hex(random_bytes(1))) : NULL;
-        $floor        = (rand(1,10) === 1) ? rand(1,20) : NULL;
-        $roomNumber   = (rand(1,10) === 1) ? strtoupper(bin2hex(random_bytes(1))) : NULL;
+        $buildingName = (($x % 13) === 0) ? 'Building ' . strtoupper(bin2hex(random_bytes(1))) : NULL;
+        $floor        = (($x % 9) === 0) ? rand(1,20) : NULL;
+        $roomNumber   = (($x % 6) === 0) ? strtoupper(bin2hex(random_bytes(1))) : NULL;
 
         // do a count on "post_codes" documents for this $isoCode
         $count = $source->post_codes->countDocuments(['countryCode' => $isoCode]);
@@ -269,42 +246,52 @@ try {
             $locality = $document->adminName1;
         }
 
-        $insert['phone'] = $phone;            // unique key
-        $insert['PrimaryContactInfo'] = [
-            'firstName'   => $first,
-            'lastName'    => $last,
-            'phoneNumber' => $phone,
-            'email'       => $email,
-            'socialMedia' => $soc
-        ];
-        $insert['LoginInfo'] = [
-            'userName' => $username,
-            'password' => password_hash($password, PASSWORD_DEFAULT),
-        ];        
-        $insert['Address'] = [
-            'streetAddressOfBuilding' =>  $address,
-            'buildingName'            =>  $buildingName,
-            'floor'                   =>  $floor,
-            'roomApartmentCondoNumber'=>  $roomNumber,
-            'city'                    =>  $city,
-            'stateProvince'           =>  $stateProv,
-            'locality'                =>  $locality,
-            'country'                 =>  $isoCode,
-            'postalCode'              =>  $postCode,
-            'GeoSpatialInfo' =>  [
-                'latitude'  =>  $latitude,
-                'longitude' =>  $longitude
+        $address = [
+            'streetAddressOfBuilding' => $streetAddr,
+            'buildingName'            => $buildingName,
+            'floor'                   => $floor,
+            'roomApartmentCondoNumber'=> $roomNumber,
+            'city'                    => $city,
+            'stateProvince'           => $stateProv,
+            'locality'                => $locality,
+            'country'                 => $isoCode,
+            'postalCode'              => $postCode,
+            'GeoSpatialInfo' => [
+                'Latitude'  => $document->latitude,
+                'Longitude' => $document->longitude
             ]
-        ];        
-        $insert['SecondaryContactInfo'] = [
-            'secondaryPhoneNumbers'    => [$phone2, $phone3],
-            'secondaryEmailAddresses'  => [$email2]
-        ];        
-        $insert['OtherInfo'] = [
-            'dateOfBirth' => $dob,
-            'gender'      => $gender,
-        ];        
+        ];
 
+        //*** build RoomInfo *********************************************************
+        /*
+        "RoomInfo" : {
+            "roomType"        : <common::roomType>,
+            "roomLocation"    : <common::roomLocation>,
+            "price"           : <float>,
+            "bedType"         : [<common::bedType>],
+            "breakfast"       : <common::breakfast>,
+            "refundable       : <common::refundable>,
+            "discount"        : <float>
+        }
+        */
+        $roomInfo = [];
+        for ($x = 0; $x < rand(2, 8); $x++ ) {
+            $roomInfo[$x]['roomType']     = $roomType[array_rand($roomType)];
+            $roomInfo[$x]['roomLocation'] = $roomType[array_rand($roomLocation)];
+            $roomInfo[$x]['price']        = (float) rand(20,1000);
+            for ($y = 0; $y < rand(1, 3); $y++ )
+                $roomInfo[$x]['bedType'][] = $bedType[array_rand($bedType)];
+            $roomInfo[$x]['breakfast']    = $breakfast[array_rand($breakfast)];
+        }
+
+        // build data to write
+        $insert = [
+            'PropertyInfo'       => $propertyInfo,
+            'PrimaryContactInfo' => $primaryContactInfo,
+            'Address'            => $address,
+            'RoomInfo'           => $roomInfo
+        ];
+        
         // write to MongoDB if flag enabled
         if ($writeBson) {
             if ($target->insertOne($insert)) {
@@ -316,6 +303,7 @@ try {
         $outputJs = $openJs . json_encode($insert, JSON_PRETTY_PRINT) . $closeJs;
         if ($writeJs) {
             $jsFile->fwrite($outputJs);
+            $written++;
         }
         echo $outputJs;
         $processed++;
@@ -324,6 +312,7 @@ try {
 
     echo $processed . ' documents processed' . PHP_EOL;
     echo $inserted  . ' documents inserted' . PHP_EOL;
+    echo $written   . ' documents written' . PHP_EOL;
 
 } catch (Exception $e) {
     echo $e->getMessage();
